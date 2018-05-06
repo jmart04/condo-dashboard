3ae830594e8dc679e71522b7487c9313fa73aa7e
Jim Martin <martj1864@umn.edu>
2018-05-06 16:06:13 -0500

    Test change 987

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload. Test123456789999
+          To get started, edit <code>src/App.js</code> and save to reload. Test123456789999123
         </p>
       </div>
     );
